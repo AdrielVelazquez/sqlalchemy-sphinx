71834aabb9de6b9098fe17bec2ccb81397e3ae8e
subnix <subnix@users.noreply.github.com>
2019-10-31 22:37:57 +0300

    FIX: typo in dialect name

--- sqlalchemy_sphinx/__init__.py
+++ sqlalchemy_sphinx/__init__.py
@@ -4,6 +4,6 @@ __version__ = "0.8"
 
 # https://bitbucket.org/zzzeek/sqlalchemy/issues/3536/engine-entrypoint-plugins
 registry.register("sphinx", "sqlalchemy_sphinx.mysqldb", "Dialect")
-registry.register("sphinx.cymsql", "sqlalchemy_sphinx.cymysql", "Dialect")
+registry.register("sphinx.cymysql", "sqlalchemy_sphinx.cymysql", "Dialect")
 registry.register("sphinx.mysqldb", "sqlalchemy_sphinx.mysqldb", "Dialect")
 registry.register("sphinx.pymysql", "sqlalchemy_sphinx.pymysql", "Dialect")
