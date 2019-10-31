89c21b6d5b54f30b386e711ad242dcff9ddb31ed
subnix <subnix@users.noreply.github.com>
2019-10-31 22:40:21 +0300

    FIX: percent char escaping

--- sqlalchemy_sphinx/utils.py
+++ sqlalchemy_sphinx/utils.py
@@ -6,6 +6,8 @@ SPECIAL_CHARS_RE = re.compile("([@()])")
 
 def escape_special_chars(match_string):
     if match_string:
+        if "%" in match_string:
+            match_string = match_string.replace("%", "%%")
         if SPECIAL_CHARS_RE.search(match_string):
             match_string = SPECIAL_CHARS_RE.sub(r"\\\\\1", match_string)
     return match_string
